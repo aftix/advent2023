b8ef7bdb434460d1ccc609bef7d57d27177f6ce1
Wyatt Campbell <wyatt.campbell@cirrus.com>
2023-12-07 11:43:09 -0600

    Reverted day1 to old parsing (day 2 changes results)

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,5 +1,22 @@
-use advent2023::parser::parse_line;
+use advent2023::parser::parse_literal_digit;
+use nom::{
+    character::complete::alpha0,
+    multi::{many1, many_till},
+    IResult,
+};
 use std::io;
+
+fn parse_glob_then_digit(input: &str) -> IResult<&str, i64> {
+    let (rest, (_, digit)) = many_till(alpha0, parse_literal_digit)(input)?;
+    Ok((rest, digit))
+}
+
+fn parse_line(input: &str) -> IResult<&str, i64> {
+    let (_, first_digit) = parse_glob_then_digit(input)?;
+    let (_, second_digit) = many1(parse_glob_then_digit)(input)?;
+    let second_digit = second_digit.last().unwrap();
+    Ok(("", first_digit * 10 + second_digit))
+}
 
 pub fn main() {
     let stdin = io::stdin();
