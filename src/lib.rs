f18dcbd5fade4fe2a2157d54e74e1a483e1b12d1
Wyatt Campbell <wyatt.campbell@cirrus.com>
2023-12-07 17:24:29 -0600

    Add peeking on last character of digit name

--- src/lib.rs
+++ src/lib.rs
@@ -3,37 +3,39 @@
 pub mod parser {
     use nom::{
         branch::alt,
-        bytes::complete::{tag_no_case, take},
-        character::complete::one_of,
+        bytes::complete::{tag, take},
+        character::complete::{char, one_of},
+        combinator::peek,
         error::{Error, ErrorKind},
         multi::{many1, many_till},
+        sequence::terminated,
         Err as nErr, IResult,
     };
 
     pub fn parse_spelled_digit(input: &str) -> IResult<&str, i64> {
         let (rest, num) = alt((
-            tag_no_case("one"),
-            tag_no_case("two"),
-            tag_no_case("three"),
-            tag_no_case("four"),
-            tag_no_case("five"),
-            tag_no_case("six"),
-            tag_no_case("seven"),
-            tag_no_case("eight"),
-            tag_no_case("nine"),
+            terminated(tag("one"), peek(char('e'))),
+            terminated(tag("tw"), peek(char('o'))),
+            terminated(tag("thre"), peek(char('e'))),
+            terminated(tag("fou"), peek(char('r'))),
+            terminated(tag("fiv"), peek(char('e'))),
+            terminated(tag("si"), peek(char('x'))),
+            terminated(tag("seve"), peek(char('n'))),
+            terminated(tag("eigh"), peek(char('8'))),
+            terminated(tag("nin"), peek(char('e'))),
         ))(input)?;
 
         let num = match num {
-            "one" => 1,
-            "two" => 2,
-            "three" => 3,
-            "four" => 4,
-            "five" => 5,
-            "six" => 6,
-            "seven" => 7,
-            "eight" => 8,
-            "nine" => 9,
-            _ => return Err(nErr::Failure(Error::new(rest, ErrorKind::Digit))),
+            "on" => 1,
+            "tw" => 2,
+            "thre" => 3,
+            "fou" => 4,
+            "fiv" => 5,
+            "si" => 6,
+            "seve" => 7,
+            "eigh" => 8,
+            "nin" => 9,
+            _ => return Err(nErr::Error(Error::new(rest, ErrorKind::Digit))),
         };
 
         Ok((rest, num))
@@ -43,7 +45,7 @@ pub mod parser {
         let (rest, num) = one_of("123456789")(input)?;
         match num.to_digit(10) {
             Some(num) => Ok((rest, num as i64)),
-            None => Err(nom::Err::Failure(Error::new(rest, ErrorKind::Digit))),
+            None => Err(nom::Err::Error(Error::new(rest, ErrorKind::Digit))),
         }
     }
 
@@ -59,8 +61,8 @@ pub mod parser {
     pub fn parse_line(input: &str) -> IResult<&str, i64> {
         let (_, first_digit) = parse_glob_then_digit(input)?;
         let (_, second_digit) = many1(parse_glob_then_digit)(input)?;
+        println!("{} {:?}", input, second_digit);
         let second_digit = second_digit.last().unwrap();
-        println!("{input} {:?}", first_digit * 10 + second_digit);
         Ok(("", first_digit * 10 + second_digit))
     }
 }
