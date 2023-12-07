7b108ca567cf1535120cf5ba8e9d8eb27e2d130e
Wyatt Campbell <wyatt.campbell@cirrus.com>
2023-12-07 10:21:13 -0600

    Moved day1 parsing to lib.rs

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,21 +1,5 @@
+use advent2023::parser::parse_line;
 use std::io;
-
-use nom::{
-    character::complete::{alpha0, one_of},
-    multi::many1,
-    sequence::preceded,
-    IResult,
-};
-
-fn parse_line(input: &str) -> IResult<&str, i64> {
-    let (_, first_digit) = preceded(alpha0, one_of("0123456789"))(input)?;
-    let (_, second_digit) = many1(preceded(alpha0, one_of("0123456789")))(input)?;
-    let second_digit = second_digit.last().unwrap();
-    Ok((
-        "",
-        (first_digit.to_digit(10).unwrap() * 10 + second_digit.to_digit(10).unwrap()) as i64,
-    ))
-}
 
 pub fn main() {
     let stdin = io::stdin();
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,20 @@
 // Advent of Code 2023 utility lib
+
+pub mod parser {
+    use nom::{
+        character::complete::{alpha0, one_of},
+        multi::many1,
+        sequence::preceded,
+        IResult,
+    };
+
+    pub fn parse_line(input: &str) -> IResult<&str, i64> {
+        let (_, first_digit) = preceded(alpha0, one_of("0123456789"))(input)?;
+        let (_, second_digit) = many1(preceded(alpha0, one_of("0123456789")))(input)?;
+        let second_digit = second_digit.last().unwrap();
+        Ok((
+            "",
+            (first_digit.to_digit(10).unwrap() * 10 + second_digit.to_digit(10).unwrap()) as i64,
+        ))
+    }
+}
