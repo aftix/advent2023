4a87d6cebca80b38d57f98cb94f4a0a11e22f14a
aftix <aftix@aftix.xyz>
2023-12-08 18:34:17 -0600

    Added github workflow

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -23,7 +23,7 @@ pub fn main() {
     let lines: Vec<String> = io::stdin().lines().flatten().collect();
     let sum: i64 = lines
         .par_iter()
-        .map(|line| parse_line(&line).ok().map(|(_, num)| num))
+        .map(|line| parse_line(line).ok().map(|(_, num)| num))
         .flatten()
         .sum();
     println!("Sum is {sum}");
