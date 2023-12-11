1923c30bc3fc4a1325a6e6aaea88d101456c1dbe
Wyatt Campbell <wyatt.campbell@cirrus.com>
2023-12-11 14:14:41 -0600

    Added script to generate binaries

--- src/bin/day3p2.rs
+++
@@ -1,7 +0,0 @@
-use std::io;
-
-pub fn main() {
-    let lines: Vec<String> = io::stdin().lines().map_while(Result::ok).collect();
-    let input: Vec<_> = lines.iter().map(String::as_str).collect();
-    println!("Sum is {}", advent2023::day3p2(&input));
-}
